90af855d78c8793eef63604874f00a041a02046a
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-09-25 15:30:27 +0700

    add code

--- ScopedServiceWithSplat/Program.cs
+++ ScopedServiceWithSplat/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Splat;
+using Splat.Microsoft.Extensions.DependencyInjection;
+
+var host = Host.CreateDefaultBuilder()
+               .ConfigureServices((_context, services) =>
+               {
+                   services.UseMicrosoftDependencyResolver();
+                   var resolver = Locator.CurrentMutable;
+                   resolver.InitializeSplat();
+
+                   services
+                    .AddScoped<DataSerivce>()
+                    .AddScoped<RandomCommand>();
+               })
+               .UseDefaultServiceProvider(config =>
+               {
+                   config.ValidateOnBuild = true;
+               })
+               .Build();
+var container = host.Services;
+container.UseMicrosoftDependencyResolver();
+
+using (var scope = container.CreateScope())
+{
+    var dataService = scope.ServiceProvider.GetRequiredService<DataSerivce>();
+    dataService.AccountId = 100;
+    Console.WriteLine(dataService.AccountId);
+
+    //var randomTask = Locator.Current.GetService<RandomCommand>();
+    var randomTask = scope.ServiceProvider.GetRequiredService<RandomCommand>();
+    randomTask?.Execute();
+}
